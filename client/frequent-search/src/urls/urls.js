3af575af9df40084ed40672355fce85ca9e43e8a
avtact <avaneesh@techesperto.com>
2025-01-21 21:09:08 +0530

    new

--- client/frequent-search/src/urls/urls.js
+++ client/frequent-search/src/urls/urls.js
@@ -1,6 +1,6 @@
 export const API_URLS = {
-  COUNTRIES: "http://localhost:5000/countries", // Backend API for countries
-  GEONAME: "http://localhost:5000/states", // Backend API for states
-  CITIES: "http://localhost:5000/cities",
-  REGISTER: "http://localhost:5000/register", // Register API
+  COUNTRIES: "https://localhost:5000/countries", // Backend API for countries
+  GEONAME: "https://localhost:5000/states", // Backend API for states
+  CITIES: "https://localhost:5000/cities",
+  REGISTER: "https://localhost:5000/register", // Register API
 };
